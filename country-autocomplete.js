795bd6a3b4dd06f2424523240a9a70108875082e
MacGyverOfCode <ultramagnus2006@yahoo.com>
2015-01-31 15:36:18 +0000

    Added country-autocomplete.js

---
+++ country-autocomplete.js
@@ -0,0 +1,47 @@
+angular.module('countryAutocompleteApp', []);
+angular.module('countryAutocompleteApp').controller('countryController', function($scope, $http){
+	
+	$scope.activeCountry = -1;
+	$scope.countrySearchTerm = '';
+	$scope.pickedCountry = '';
+	$scope.countryList = [];
+
+	$http.get('country-list.json').then(function(res){
+		$scope.countryList = res.data.countries;
+	});
+
+	$scope.countrySearch = function() {
+		var result = '!';
+		if ($scope.countrySearchTerm != '') {
+			if ($scope.countrySearchTerm != $scope.pickedCountry) {
+				result = $scope.countrySearchTerm;
+			};
+		};
+		return result;
+	};
+
+	$scope.checkKeyPresses = function(e) {
+		if (e === 40) {
+			$scope.activeCountry++;
+			if ($scope.activeCountry === $scope.filtered.length) {
+				$scope.activeCountry--;
+			};
+		} else if (e === 38) {
+			$scope.activeCountry--;
+			if ($scope.activeCountry === -1) {
+				$scope.activeCountry++;
+			};
+		} else if (e === 13 && $scope.activeCountry > -1) {
+			$scope.pickCountry($scope.filtered[$scope.activeCountry].longname);
+			$scope.activeCountry = -1;
+		} else {
+			$scope.activeCountry = -1;
+		};
+	};
+
+	$scope.pickCountry = function(country) {
+		$scope.countrySearchTerm = country;
+		$scope.pickedCountry = country;
+	};
+
+});
