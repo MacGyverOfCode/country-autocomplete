021d9a40dd1b9cc5d0ba8d5588d392e7f7cb2a01
MacGyverOfCode <richard_a_roth@yahoo.com>
2017-06-08 09:43:29 +0100

    Rewritten in ES6

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # country-autocomplete
-Text field that autocompletes any country name. Requires AngularJS 1.3
+Text field that autocompletes any country name. Written in ES6 JavaScript.
 
-Edit country-list.json to edit any country name. "Shortnames" should include the two digit country code as well as any other names that might be used (ie "Holland" or "Nederland" for The Netherlands).
+Edit country-list.json to change any country name. "Shortnames" should include the two digit country code as well as any other names that might be used (ie "Holland" or "Nederland" for The Netherlands).
--- country-autocomplete.js
+++ country-autocomplete.js
@@ -1,50 +1,139 @@
-(function() {
+'use strict';
 
-	angular.module('countryAutocompleteApp', []).controller('countryController', function($scope, $http){
-		
-		$scope.activeCountry = -1;
-		$scope.countrySearchTerm = '';
-		$scope.pickedCountry = '';
-		$scope.countryList = [];
+class CountryAutocomplete {
 
-		$http.get('country-list.json').then(function(res){
-			$scope.countryList = res.data.countries;
+	constructor(input) {
+		if (this.checkInput(input)) {
+			this.activeCountry = -1;
+			this.input = input;
+			this.activeCountry = 
+			this.countryList = [];
+			this.suggestedCountries = [];
+			this.loadCountryList();
+			this.createContainer();
+			this.createSuggestBox();
+			this.bindSearch();
+			this.bindKeyPress();
+		};
+	}
+
+	addClickEvent(item, country) {
+		item.addEventListener('click', () => {
+			this.selectCountry(country);
 		});
+	}
 
-		$scope.countrySearch = function() {
-			var result = '!';
-			if ($scope.countrySearchTerm != '') {
-				if ($scope.countrySearchTerm != $scope.pickedCountry) {
-					result = $scope.countrySearchTerm;
+	bindKeyPress() {
+		this.input.addEventListener('keydown', (e) => {			
+			if (e.keyCode === 40) {
+				this.activeCountry++;
+				if (this.activeCountry === this.suggestedCountries.length) {
+					this.activeCountry--;
 				};
-			};
-			return result;
-		};
-
-		$scope.checkKeyPresses = function(e) {
-			if (e === 40) {
-				$scope.activeCountry++;
-				if ($scope.activeCountry === $scope.filtered.length) {
-					$scope.activeCountry--;
+			} else if (e.keyCode === 38) {
+				this.activeCountry--;
+				if (this.activeCountry === -1) {
+					this.activeCountry++;
 				};
-			} else if (e === 38) {
-				$scope.activeCountry--;
-				if ($scope.activeCountry === -1) {
-					$scope.activeCountry++;
-				};
-			} else if (e === 13 && $scope.activeCountry > -1) {
-				$scope.pickCountry($scope.filtered[$scope.activeCountry].longname);
-				$scope.activeCountry = -1;
+			} else if (e.keyCode === 13 && this.activeCountry > -1) {
+				this.selectCountry(this.suggestedCountries[this.activeCountry]);
+				this.activeCountry = -1;
 			} else {
-				$scope.activeCountry = -1;
+				this.activeCountry = -1;
+			};
+			this.highlightCountry();
+		});
+	}
+
+	bindSearch() {
+		this.input.addEventListener('input', (e) => {
+			this.searchCountries(this.input);
+		});
+	}
+
+	checkInput(input) {
+		if (typeof(input) === 'undefined') {
+			console.error('Sorry, but an input element must be specified.');
+			return false;
+		} else {
+			return true;
+		};
+	}
+
+	createContainer() {
+		let container = document.createElement('div');
+		container.classList.add('country-autocomplete');
+		this.input.parentNode.insertBefore(container, this.input);
+		container.appendChild(this.input);
+	}
+
+	createSuggestBox() {
+		let container = document.querySelector('.country-autocomplete');
+		let suggest = document.createElement('div');
+		let list = document.createElement('ul');
+		suggest.classList.add('country-autocomplete-suggest');
+		list.classList.add('country-autocomplete-suggest-list');
+		suggest.appendChild(list);
+		container.appendChild(suggest);
+	}
+
+	highlightCountry() {
+		let index = 0;
+		let items = document.querySelectorAll('.country-autocomplete-suggest-item');
+		[ ...items ].forEach((item) => {
+			if (index === this.activeCountry) {
+				item.classList.add('active');
+			} else {
+				item.classList.remove('active');
+			};
+			index++;
+		});
+	}
+
+	loadCountryList() {
+		fetch('country-list.json')
+		.then((response) => response.json())
+		.then((data) => {
+			this.countryList = data.countries;
+		});
+	}
+
+	populateSuggestions() {
+		let list = document.querySelector('.country-autocomplete-suggest-list');
+		list.innerHTML = '';
+		for (let country of this.suggestedCountries) {
+			let item = document.createElement('li');
+			item.classList.add('country-autocomplete-suggest-item');
+			item.textContent = country;
+			this.addClickEvent(item, country);
+			list.appendChild(item);
+		};
+	}
+
+	searchCountries(input) {
+		this.suggestedCountries = [];
+		for (let country of this.countryList) {
+			if (input.value.length > 0) {
+				let inputValue = input.value.toLowerCase();
+				let longname = country.longname.toLowerCase();
+				let shortnames = null;
+				if (country.shortnames) {
+					shortnames = country.shortnames.toLowerCase();
+				};
+				if ( (longname.includes(inputValue)) || 
+					(shortnames && shortnames.includes(inputValue)) ) {
+					this.suggestedCountries.push(country.longname);
+				};
 			};
 		};
+		this.populateSuggestions();
+	}
 
-		$scope.pickCountry = function(country) {
-			$scope.countrySearchTerm = country;
-			$scope.pickedCountry = country;
-		};
+	selectCountry(country) {
+		let input = document.querySelector('.js-country-autocomplete');
+		let list = document.querySelector('.country-autocomplete-suggest-list');
+		input.value = country;
+		list.innerHTML = '';
+	}
 
-	});
-
-})();
+};
